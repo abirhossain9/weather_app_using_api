c55b326d044fee75887322021d20851467c277b1
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-08-05 16:12:41 +0600

    Api Infos
    Api Infos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,3 @@
-// Write your javascript code here
+let weatherAPIKey = "d6871fcb9cce814f23b29dbb8fe7c079";
+let weatherBaseEndpoint =
+  "https://api.openweathermap.org/data/2.5/weather?appid=${weatherAPIKey}";
