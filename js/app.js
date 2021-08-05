5283ed1b3c008a630a54e0a4b0dac235dabd879d
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-08-05 16:40:20 +0600

    testing api
    testing api

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 let weatherAPIKey = "d6871fcb9cce814f23b29dbb8fe7c079";
-let weatherBaseEndpoint =
-  "https://api.openweathermap.org/data/2.5/weather?appid=${weatherAPIKey}";
+let weatherBaseEndpoint = `https://api.openweathermap.org/data/2.5/weather?appid=${weatherAPIKey}`;
+
+//data fetch from api
+const getWeatherDataByCity = async (city) => {
+  const endpoint = `${weatherBaseEndpoint}&q=${city}`;
+  let request = await fetch(endpoint);
+  let data = await request.json();
+  console.log(data);
+};
+
+getWeatherDataByCity("dhaka");
